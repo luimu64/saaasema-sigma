fc8d3cd93ec97c5743c67cfa1d639ae66fbcc82a
migus <187182959+minfux@users.noreply.github.com>
2025-03-06 11:54:25 +0200

    --amend

--- analogdigital.ino
+++ analogdigital.ino
@@ -1,11 +1,5 @@
 constexpr uint8_t MAGIC_PIN = A0;
 
-
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
 void setup()
 {
   Serial.begin(9600);
