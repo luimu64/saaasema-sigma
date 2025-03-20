f69bb7e5a37a6773ccb48645a2fe627c0c414fa5
ItsDuska <89298953+ItsDuska@users.noreply.github.com>
2025-03-20 10:31:06 +0200

    ViikoTeht3 + networking stuff
    ööh vissiin

--- analogdigital/analogdigital.ino
+++ analogdigital/analogdigital.ino
@@ -1,4 +1,6 @@
 #include <LiquidCrystal.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
 
 constexpr uint8_t MAGIC_PIN = A0;
 constexpr float vcc = 4.77;
@@ -25,6 +27,21 @@ constexpr byte customChar[8] = {
 const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+
+// SERVER STUFF
+byte server[] = { 10,6,0,21 }; // MQTT-palvelimen IP-osoite
+unsigned int Port = 1883;  // MQTT-palvelimen portti
+EthernetClient ethClient; // Ethernet-kirjaston client-olio
+PubSubClient client(server, Port, ethClient); // PubSubClient-olion luominen
+
+#define outTopic   "ICT4_out_2020" // Aihe, jolle viesti lähetetään
+
+static uint8_t mymac[6] = { 0x44,0x76,0x58,0x10,0x00,0x62 }; // MAC-osoite Ethernet-liitäntää varten
+
+char* clientId = "a731fsd4"; // MQTT-clientin tunniste
+
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -32,6 +49,7 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(2), interupt, RISING);
   lcd.begin(20, 4);
   lcd.createChar(0,customChar);
+  fetch_IP();
 }
 
 void interupt()
@@ -99,6 +117,42 @@ void printData()
   lcd.print(" m/s");
 }
 
+
+
+void fetch_IP() {
+    bool connectionSuccess = Ethernet.begin(mymac); // Yhdistäminen Ethernet-verkkoon ja tallennetaan yhteyden tila
+    if (!connectionSuccess) {
+        Serial.println("Failed to access Ethernet controller"); // Jos yhteys ei onnistunut -> yhteysvirheilmoitus
+    } else {
+        Serial.println("Connected with IP: " + Ethernet.localIP()); // Onnistuessa tulostetaan IP-osoite
+    }
+}
+
+void send_MQTT_message() {
+    if (!client.connected()) { // Tarkistetaan onko yhteys MQTT-brokeriin muodostettu
+        connect_MQTT_server(); // Jos yhteyttä ei ollut, kutsutaan yhdistä -funktiota
+    }
+    if (client.connected()) { // Jos yhteys on muodostettu
+        client.publish(outTopic, "Hello from MQTT!"); // Lähetetään viesti MQTT-brokerille
+        Serial.println("Message sent to MQTT server."); // Tulostetaan viesti onnistuneesta lähettämisestä
+    } else {
+        Serial.println("Failed to send message: not connected to MQTT server."); // Ei yhteyttä -> Yhteysvirheilmoitus
+    }
+}
+
+void connect_MQTT_server() { 
+    Serial.println("Connecting to MQTT"); // Tulostetaan vähän info-viestiä
+    if (client.connect(clientId)) { // Tarkistetaan saadaanko yhteys MQTT-brokeriin
+        Serial.println("Connected OK"); // Yhdistetty onnistuneesti
+    } else {
+        Serial.println("Connection failed."); // Yhdistäminen epäonnistui
+    }    
+}
+
+
+
+
+
 void loop()
 {
   static unsigned long lastMillis = 0;
@@ -121,5 +175,7 @@ void loop()
   digitalDataMapIndex = (digitalDataMapIndex + 1) % MAX_COUNT; 
   pulse = 0; 
 
-  printData();  
+  printData();
+  send_MQTT_message(); // Kutsutaan MQTT-viestin lähettämis-funktiota
+
 }
