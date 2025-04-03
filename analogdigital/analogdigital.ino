4935eba26946176470351fb3b231ab4ea454a35b
luimu64 <luimu@luimu.dev>
2025-04-03 12:22:52 +0300

    fixed analog from going negative because of our vibe coding
    Co-authored-by: Minfux <amberwillow@selenemail.com>

--- analogdigital/analogdigital.ino
+++ analogdigital/analogdigital.ino
@@ -2,11 +2,18 @@
 #include <LiquidCrystal.h>
 #include <PubSubClient.h>
 
-constexpr uint8_t MAGIC_PIN = A0;
+#define outTopic "ICT_2021"        // Aihe, jolle viesti lähetetään
+char *clientId = "sigma-24TIETOA"; // MQTT-clientin tunniste
+char *deviceId = "sigma";          // Laitteen tunniste
+char *deviceSecret = "tamk";
+
+constexpr uint8_t ANAL_PIN = A0;
+volatile byte pulse = 0;
+constexpr byte DIG_PIN = 2;
 constexpr float vcc = 4.7;
 constexpr int MAX_COUNT = 10;
-
-volatile byte pulse = 0;
+constexpr byte customChar[8] = {B00000, B01000, B10100, B01000,
+                                B00000, B00000, B00000, B00000};
 
 float digitalDataMap[MAX_COUNT]{};
 float analogDataMap[MAX_COUNT]{};
@@ -14,28 +21,18 @@ int analogDataMapIndex = 0;
 int digitalDataMapIndex = 0;
 char buffer[190];
 
-constexpr byte customChar[8] = {B00000, B01000, B10100, B01000,
-                                B00000, B00000, B00000, B00000};
-
-const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
 // SERVER STUFF
 byte server[] = {10, 6, 0, 23};               // MQTT-palvelimen IP-osoite
 unsigned int Port = 1883;                     // MQTT-palvelimen portti
 EthernetClient ethClient;                     // Ethernet-kirjaston client-olio
 PubSubClient client(server, Port, ethClient); // PubSubClient-olion luominen
-
-#define outTopic "ICT_2021" // Aihe, jolle viesti lähetetään
-
 static uint8_t mymac[6] = {0xA8, 0x61, 0x0A, 0xAE,
                            0x46, 0x8B}; // MAC-osoite Ethernet-liitäntää varten
 
-char *clientId = "sigma-24TIETOA"; // MQTT-clientin tunniste
-char *deviceId = "sigma";          // Laitteen tunniste
-char *deviceSecret = "tamk";
+const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void interupt() { pulse++; }
+void interrupt() { pulse++; }
 
 int calculateAverage(const float *array) {
   float a = 0.0;
@@ -46,8 +43,7 @@ int calculateAverage(const float *array) {
 }
 
 const char *getDirectionStr(int direction) {
-  if ((direction >= 0 && direction < 45) ||
-      (direction >= 315 && direction <= 360)) {
+  if (direction >= 0 && direction < 45 || direction == 360) {
     return " N";
   } else if (direction >= 45 && direction < 90) {
     return " NE";
@@ -58,8 +54,10 @@ const char *getDirectionStr(int direction) {
   } else if (direction >= 180 && direction < 225) {
     return " S";
   } else if (direction >= 225 && direction < 270) {
-    return " SW";
+    return " EW";
   } else if (direction >= 270 && direction < 315) {
+    return " W";
+  } else if (direction >= 315 && direction < 359) {
     return " NW";
   }
 
@@ -118,8 +116,9 @@ void connect_MQTT_server() {
 
 void setup() {
   Serial.begin(9600);
-  pinMode(MAGIC_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(2), interupt, RISING);
+  pinMode(ANAL_PIN, INPUT);
+  pinMode(DIG_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING);
   lcd.begin(20, 4);
   lcd.createChar(0, customChar);
   fetch_IP();
@@ -137,10 +136,9 @@ void loop() {
   lastMillis = millis();
 
   const float voltage =
-      ((static_cast<float>(analogRead(MAGIC_PIN)) * (vcc / 1023)));
-  Serial.println("Voltage: " + String(voltage) + " V");
-  const float windDirection = 94.5703 * voltage - 9;
-  Serial.println("Wind direction: " + String(windDirection) + " degrees");
+      ((static_cast<float>(analogRead(ANAL_PIN)) * (vcc / 1023)));
+  // values found by educated guesses (randomized testing)
+  const float windDirection = max(0.0f, 94.5703 * voltage - 9);
   analogDataMap[analogDataMapIndex] = windDirection;
 
   analogDataMapIndex = (analogDataMapIndex + 1) % MAX_COUNT;
