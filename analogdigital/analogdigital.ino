2b66d50fc8890509a5168ae6758982d8d505044a
ItsDuska <89298953+ItsDuska@users.noreply.github.com>
2025-04-10 09:42:05 +0300

    code cleanup

--- analogdigital/analogdigital.ino
+++ analogdigital/analogdigital.ino
@@ -2,16 +2,30 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
 
+<<<<<<< Updated upstream
 constexpr uint8_t MAGIC_PIN = A0;
 constexpr float vcc = 4.77;
+=======
+
+#define outTopic "ICT4_out_2020"        // Aihe, jolle viesti lähetetään
+constexpr char *clientId = "sigma-24TIETOA"; // MQTT-clientin tunniste
+constexpr char *deviceId = "sigma";          // Laitteen tunniste
+constexpr char *deviceSecret = "tamk";
+
+constexpr uint8_t ANALOG_PIN = A0;
+constexpr byte DIGITAL_PIN = 2;
+constexpr float vcc = 4.7;
+>>>>>>> Stashed changes
 constexpr int MAX_COUNT = 10;
 
 volatile byte pulse = 0;
 
 float digitalDataMap[MAX_COUNT]{};
 float analogDataMap[MAX_COUNT]{};
+volatile byte pulse = 0;
 int analogDataMapIndex = 0;
 int digitalDataMapIndex = 0;
+<<<<<<< Updated upstream
 
 constexpr byte customChar[8] = {
   B00000,
@@ -23,10 +37,32 @@ constexpr byte customChar[8] = {
   B00000,
   B00000
 };
+=======
+bool messageDedicator = false;
 
-const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+
+// SERVER STUFF
+byte server[] = {10, 6, 0, 23};               // MQTT-palvelimen IP-osoite
+constexpr unsigned int port = 1883;                     // MQTT-palvelimen portti
+EthernetClient ethernetClient;                     // Ethernet-kirjaston client-olio
+PubSubClient client(server, port, ethernetClient); // PubSubClient-olion luominen
+constexpr uint8_t myMAC[6] = {0xA8, 0x61, 0x0A, 0xAE,
+                           0x46, 0x8B}; // MAC-osoite Ethernet-liitäntää varten
+>>>>>>> Stashed changes
+
+
+// LCD SCREEN
+constexpr int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+<<<<<<< Updated upstream
+=======
+
+const char* jsonLayoutStr = "IOTJS={\"S_name\":\"sigma-ts\",\"S_value\":";
+
+
+void interrupt() { pulse++; }
+>>>>>>> Stashed changes
 
 // SERVER STUFF
 byte server[] = { 10,6,0,21 }; // MQTT-palvelimen IP-osoite
@@ -120,12 +156,25 @@ void printData()
 
 
 void fetch_IP() {
+<<<<<<< Updated upstream
     bool connectionSuccess = Ethernet.begin(mymac); // Yhdistäminen Ethernet-verkkoon ja tallennetaan yhteyden tila
     if (!connectionSuccess) {
         Serial.println("Failed to access Ethernet controller"); // Jos yhteys ei onnistunut -> yhteysvirheilmoitus
     } else {
         Serial.println("Connected with IP: " + Ethernet.localIP()); // Onnistuessa tulostetaan IP-osoite
     }
+=======
+  bool connectionSuccess = Ethernet.begin(
+      myMAC); // Yhdistäminen Ethernet-verkkoon ja tallennetaan yhteyden tila
+  if (!connectionSuccess) {
+    Serial.println(
+        "Failed to access Ethernet controller"); // Jos yhteys ei onnistunut ->
+                                                 // yhteysvirheilmoitus
+  } else {
+    Serial.println("Connected with IP: " +
+                   Ethernet.localIP()); // Onnistuessa tulostetaan IP-osoite
+  }
+>>>>>>> Stashed changes
 }
 
 void send_MQTT_message() {
@@ -149,6 +198,18 @@ void connect_MQTT_server() {
     }    
 }
 
+<<<<<<< Updated upstream
+=======
+void setup() {
+  Serial.begin(9600);
+  pinMode(ANALOG_PIN, INPUT);
+  pinMode(DIGITAL_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING);
+  lcd.begin(20, 4);
+  lcd.createChar(0, customChar);
+  fetch_IP();
+}
+>>>>>>> Stashed changes
 
 
 
@@ -165,8 +226,15 @@ void loop()
 
   lastMillis = millis();
 
+<<<<<<< Updated upstream
   const float voltage = (static_cast<float>(analogRead(MAGIC_PIN)) / 1023) * vcc;
   const float windDirection = 94.5703 * voltage + 0.1586;
+=======
+  const float voltage =
+      ((static_cast<float>(analogRead(ANALOG_PIN)) * (vcc / 1023)));
+  // values found by educated guesses (randomized testing)
+  const float windDirection = max(0.0f, 94.5703 * voltage - 9);
+>>>>>>> Stashed changes
   analogDataMap[analogDataMapIndex] = windDirection;
   Serial.println(windDirection);
 
@@ -178,4 +246,18 @@ void loop()
   printData();
   send_MQTT_message(); // Kutsutaan MQTT-viestin lähettämis-funktiota
 
+<<<<<<< Updated upstream
+=======
+  const String firstMessage = String(jsonLayoutStr + String(direction) + "}");
+  const String secondMessage = String(jsonLayoutStr + String(speed) + "}");
+
+  // Kutsutaan MQTT-viestin lähettämis-funktiota
+  messageDedicator = !messageDedicator;
+  if (messageDedicator){
+    send_MQTT_message(firstMessage);
+    return;
+  }
+
+  send_MQTT_message(secondMessage);
+>>>>>>> Stashed changes
 }
